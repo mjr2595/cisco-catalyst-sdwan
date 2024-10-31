2fa49ce0b713ed1600c6ff55b835d0cb9cde2f6f
mjr2595 <michaeljohnraymond@gmail.com>
2024-10-31 18:45:35 -0500

    updated reqs, formatting

--- login.py
+++ login.py
@@ -1,22 +1,24 @@
-import requests
 import sys
 
+import requests
+
+
 def login(vmanage_ip, username, password):
-        """Login to vmanage"""
-        base_url_str = 'https://%s:8443/'%vmanage_ip
+    """Login to vmanage"""
+    base_url_str = "https://%s:8443/" % vmanage_ip
 
-        login_action = '/j_security_check'
+    login_action = "/j_security_check"
 
-        #Format data for loginForm
-        login_data = {'j_username' : username, 'j_password' : password}
+    # Format data for loginForm
+    login_data = {"j_username": username, "j_password": password}
 
-        #Url for posting login data
-        login_url = base_url_str + login_action
+    # Url for posting login data
+    login_url = base_url_str + login_action
 
-        sess = requests.session()
-        #If the vmanage has a certificate signed by a trusted authority change verify to True
-        login_response = sess.post(url=login_url, data=login_data, verify=False)
+    sess = requests.session()
+    # If the vmanage has a certificate signed by a trusted authority change verify to True
+    login_response = sess.post(url=login_url, data=login_data, verify=False)
 
-        if b'<html>' in login_response.content:
-            print ("Login Failed")
-            sys.exit(0)
+    if b"<html>" in login_response.content:
+        print("Login Failed")
+        sys.exit(0)
